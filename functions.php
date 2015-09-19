32781286f5d6b21ab867cdd0d97757ef4c4c499c
Devio Digital <deviodigital@gmail.com>
2015-09-19 07:45:35 -0400

    added code to change login page logo with logo you upload in the customizer

--- functions.php
+++ functions.php
@@ -255,3 +255,43 @@ function excerpt($limit) {
   $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
   return $excerpt;
 }
+
+/**
+ * Using logo upload from customizer to change login page logo
+ */
+
+if ( get_theme_mod( 'peddle_logo' ) )  {
+
+// Change login page logo image to the logo uploaded in the customizer	
+function peddle_login_logo() {
+	
+	list($width,$height) = getimagesize(get_theme_mod( "peddle_logo" ));
+
+	echo '<style type="text/css">
+		#login {
+			width: '. $width .'px !important;
+		}
+		h1 a {
+			background-image:url('. get_theme_mod( "peddle_logo" ) .') !important;
+			background-size: '. $width .'px !important;
+			width: '. $width .'px !important;
+			height: '. $height .'px !important;
+		}
+	</style>';
+}
+
+add_action('login_head', 'peddle_login_logo');
+
+// Change login page logo link to the website home page
+function peddle_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'peddle_login_logo_url' );
+
+// Change login page logo title to the website name
+function peddle_login_logo_url_title() {
+    return get_bloginfo( 'name' );
+}
+add_filter( 'login_headertitle', 'peddle_login_logo_url_title' );
+
+} // if get_theme_mod('peddle_logo')
