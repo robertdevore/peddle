422cded02ce29129c0a60b7e321e759717438c1e
Devio Digital <deviodigital@gmail.com>
2015-10-06 09:20:41 -0400

    Updated textdomain conflicts

--- archive-download.php
+++ archive-download.php
@@ -72,11 +72,11 @@ get_header(); ?>
 						<?php endif; ?>
 
 						<?php if ( edd_has_variable_prices( get_the_ID() ) ) : ?>
-							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Starting at: ', 'vendd'); edd_price( get_the_ID() ); ?></span></a>
+							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Starting at: ', 'peddle'); edd_price( get_the_ID() ); ?></span></a>
 						<?php elseif ( '0' != edd_get_download_price( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) : ?>	
-							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Price: ', 'vendd' ); edd_price( get_the_ID() ); ?></span></a>
+							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Price: ', 'peddle' ); edd_price( get_the_ID() ); ?></span></a>
 						<?php else : ?>
-							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Free Download','vendd' ); ?></span></a>
+							<a class="product-price-wrap" href="<?php the_permalink(); ?>"><span class="product-price"><?php _e( 'Free Download','peddle' ); ?></span></a>
 						<?php endif; ?>
 
 					</div><!-- /.product-description -->
